73d8c9a1c73ed73f3b207cc68383dd356e164e00
Iskhak Asanov <asanoviskhak@unist.ac.kr>
2020-09-11 18:05:03 +0900

    Refactoring and ither changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from 'react';
+import { render } from 'react-dom';
+import { Pet } from './Pet';
 
 const App = () => {
   return React.createElement(
@@ -14,7 +10,7 @@ const App = () => {
       id: "pet_list",
     },
     [
-      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement("h1", {}, "Adopt Us!"),
       React.createElement(Pet, {
         name: "Luna",
         animal: "Dog",
@@ -33,4 +29,5 @@ const App = () => {
     ]
   );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+render(React.createElement(App), document.getElementById("root"));
