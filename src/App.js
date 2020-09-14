19b7a88ec65b6feea0ee6a808d24e269e7fd7ee8
Iskhak Asanov <asanov.ishak1999@gmail.com>
2020-09-14 21:22:38 +0900

    Async calls to Petfinder API. Pet component updating

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import SearchParams from './SearchParams';
 
 const App = () => {
   return (
+    <React.StrictMode>
     <div id="pet-list" >
       <h1>Adopt Me!</h1>
       <SearchParams />
     </div>
+    </React.StrictMode>
   );
 };
 
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from './Results';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -8,25 +9,36 @@ const SearchParams = () => {
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
     const [breeds, setBreeds] = useState([]);
     const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+    const [pets, setPets] = useState([]);
+
+    async function requestPets(){
+        const {animals} = await pet.animals({
+            location,
+            breed,
+            type: animal
+        })
+        setPets(animals || [])
+    }
+
+
 
     useEffect(() => {
         setBreeds([]);
         setBreed("");
-        pet.breeds(animal).then(({ breeds }) => {
+        pet.breeds(animal).then(({  breeds }) => {
             const breedStrings = breeds.map(({ name }) => name);
             setBreeds(breedStrings);
         }, console.error);
 
-    }, [animal, setBreed, setBreeds]);
-
-    useEffect(() => {
-        document.getElementById("s-query").innerHTML = location;
-    }, [location]);
+    }, [animal, setBreed, setBreeds]); 
 
     return (
         <div className="search-params">
             <h1>{location}</h1>
-            <form>
+            <form onSubmit={(e)=>{
+                e.preventDefault();
+                requestPets();
+            }}>
                 <label htmlFor="location">
                     Location
                     <input
@@ -43,6 +55,7 @@ const SearchParams = () => {
                 </label>
                 <button>Submit</button>
             </form>
+            <Results pets = {pets}/>
         </div>
     );
 }
